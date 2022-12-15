9312ce0dc0ba9409a42959d5bf38f360b666b822
Bilal <bilaalahmaad1@gmail.com>
2022-12-15 12:43:47 +0500

    server setup using express in node #1

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.port || 3000;
+
+app.listen(port);
+
+console.log('Sever is running on the port ' + port);
+
+app.get('/', function (req, res) {
+ return res.json('Hello from the server! 🚀 🚀 🚀');
+});
+
